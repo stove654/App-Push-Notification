883224ece1ee1e406d71bfaa5d676e31b05e151e
Hoang Loi <hoangloi@Hoangs-MacBook-Pro.local>
2015-10-08 02:04:30 +0700

    update all

--- app/scripts/controllers/push_notification.js
+++ app/scripts/controllers/push_notification.js
@@ -11,11 +11,13 @@ angular.module('PushApp')
   .controller('PushNotificationCtrl', function ($scope, $modal, PushNotificationFactory, toaster, $firebaseArray, APP_CONFIG, cfpLoadingBar) {
 
     var list = $firebaseArray(new Firebase(APP_CONFIG.fireBaseUrl + 'news'));
+    var rooms = $firebaseArray(new Firebase(APP_CONFIG.fireBaseUrl + 'rooms'));
 
     $scope.messages = list;
+    $scope.rooms = rooms;
     $scope.totalItems = 64;
     $scope.currentPage = 4;
-
+    $scope.data = {};
     $scope.setPage = function (pageNo) {
       $scope.currentPage = pageNo;
     };
@@ -24,18 +26,26 @@ angular.module('PushApp')
     $scope.bigTotalItems = 175;
     $scope.bigCurrentPage = 1;
 
-    $scope.open = function (size) {
+    $scope.selectedRoom = function (item) {
+      console.log(item);
+    }
 
+    $scope.open = function (size) {
+      $scope.data = {}
       var modalInstance = $modal.open({
         animation: true,
         templateUrl: './views/states/push_notification_modal.html',
         size: 'md',
-        controller: 'ModalPushNotificationCtrl'
+        controller: 'ModalPushNotificationCtrl',
+        scope: $scope
       });
 
       modalInstance.result.then(function (params) {
         cfpLoadingBar.start();
+        $scope.data.room = JSON.parse($scope.data.room);
         params.createAt = new Date();
+        params.roomId = $scope.data.room.$id;
+        params.roomName = $scope.data.room.name;
         PushNotificationFactory.pushNotification(params).then(function (res) {
           list.$add(res.data).then(function (data) {
             toaster.pop('success', res.message);
@@ -68,6 +78,7 @@ angular.module('PushApp')
     };
 
     $scope.cancel = function () {
+      console.log($scope.data.room)
       $modalInstance.dismiss('cancel');
     };
   });
